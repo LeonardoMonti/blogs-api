3a9491e3e8a2b534c2f9ae5c5d86e7aec1e5092e
LeonardoMonti <LeonardoMonti>
2022-07-18 14:35:33 -0300

    feat: add user model req 2

---
+++ src/database/models/user.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      type: DataTypes.STRING
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    image: {
+      type: DataTypes.STRING
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'Users',
+  });
+};
