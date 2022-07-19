3f9046c5291815b7115c46b887a237817176752c
LeonardoMonti <LeonardoMonti>
2022-07-19 19:36:10 -0300

    fix: timestamps change value to true

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -29,7 +29,7 @@ module.exports = (sequelize, DataTypes) => {
     },
   },
   {
-    timestamps: false,
+    timestamps: true,
     tableName: 'BlogPosts',
     createdAt: 'published',
     updatedAt: 'updated'
